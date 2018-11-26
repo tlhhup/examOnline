21e72905d0d71677769551e0a3c312ef07393e00
tlhhup <137273278@qq.com>
2018-11-26 23:53:09 +0800

    1.添加TokenHolder来存储token值

---
+++ exam-auth/auth-commons/src/main/java/org/tlh/exam/auth/util/TokenHolder.java
@@ -0,0 +1,44 @@
+package org.tlh.exam.auth.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+
+/**
+ * Created by 离歌笑tlh/hu ping on 2018/11/26
+ * <p>
+ * Github: https://github.com/tlhhup
+ */
+@Slf4j
+public final class TokenHolder {
+
+    private TokenHolder() {
+    }
+
+    private static ThreadLocal<String> requestVariables = new InheritableThreadLocal<>();
+
+    /**
+     * 初始化token
+     * @param token
+     */
+    public static void initializeToken(String token) {
+        requestVariables.set(token);
+    }
+
+    /**
+     * 获取token
+     * @return
+     */
+    public static String getTokenForCurrentThread() {
+        String token = requestVariables.get();
+        return Optional.ofNullable(token).orElse(null);
+    }
+
+    /**
+     * 清除token
+     */
+    public static void cleanTokenForCurrentThread(){
+        requestVariables.remove();
+    }
+
+}
