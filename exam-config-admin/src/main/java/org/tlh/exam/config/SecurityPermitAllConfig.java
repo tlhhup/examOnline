55b574b8b201e9cef24b4f099308a16d732940a1
tlhhup <137273278@qq.com>
2019-01-19 11:51:51 +0800

    1.配置中心Rest服务添加到monitor中 2.升级配置中心Spring boot和scca的版本

---
+++ exam-config-admin/src/main/java/org/tlh/exam/config/SecurityPermitAllConfig.java
@@ -0,0 +1,21 @@
+package org.tlh.exam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by 离歌笑tlh/hu ping on 2019/1/14
+ * <p>
+ * Github: https://github.com/tlhhup
+ */
+@Configuration
+public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+
+}
